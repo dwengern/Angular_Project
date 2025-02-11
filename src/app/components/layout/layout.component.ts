3eedd24937ec4c4606bdc81257fc0dfddc693c7f
dwengern <dwegnern@uindy.edu>
2025-02-11 17:32:51 -0500

    Added basic login page with guards implemented to app routes.

---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterLink, RouterOutlet],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+  router = inject(Router)
+
+  onLogout() {
+    this.router.navigateByUrl('/login');
+    localStorage.removeItem('empErpUser');
+  }
+}
